979b538432ac369b1683830ba07e0ebb276063c9
mizhilaowang <comeonwwl@gmail.com>
2019-04-25 15:42:46 +0800

    daily commit

--- spider_bidcenter.py
+++ spider_bidcenter.py
@@ -1,3 +1,10 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
+# Created Date: 2019-04-09
+# Project     : crawl data of bidcenter.com.cn 
 
+import sys
+
+
+
+print("The python version is : ",sys.version)
